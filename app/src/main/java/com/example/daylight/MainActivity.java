ffff0820d10d2e6382456089455150672abacd89
inan <ghjklla007@naver.com>
2019-12-05 03:03:58 +0900

    add floatingActionButton add swipeLayout

--- app/src/main/java/com/example/daylight/MainActivity.java
+++ app/src/main/java/com/example/daylight/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.daylight;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ListView;
 
 import com.google.android.material.tabs.TabLayout;
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,20 +24,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager pager = findViewById(R.id.viewpager);
+        //Intent intent = new Intent(MainActivity.this, ListDemoActivity.class);
+        //startActivity(intent);
 
+        ViewPager pager = findViewById(R.id.viewpager);
         // 미리 캐싱 - 준비 해놓을 프레그먼트 개수
         pager.setOffscreenPageLimit(3);
 
         // 프래그먼트 매니저 생성과 동시에 어댑터에 할당 즉, 매니저=어댑터
         PagerAdapter pa = new PagerAdapter(getSupportFragmentManager());
-
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
 
         Center_main_Fragment cmf = new Center_main_Fragment();
         Right_main_Fragment rmf = new Right_main_Fragment();
         Left_main_Fragment lmf = new Left_main_Fragment();
-
         pa.addItem(lmf);
         pa.addItem(cmf);
         pa.addItem(rmf);
@@ -40,31 +45,28 @@ public class MainActivity extends AppCompatActivity {
         pager.setAdapter(pa);
         pager.setCurrentItem(1);
         tabs.setupWithViewPager(pager);
-    }
 
+
+    }
     // 프레그먼트 커스텀 어댑터
     class PagerAdapter extends FragmentStatePagerAdapter{
-
         //프래그먼트가 들어갈 어레이 리스트
         ArrayList<Fragment> page = new ArrayList<Fragment>();
-
         public PagerAdapter(FragmentManager fm){
             super(fm);
         }
-
         public void addItem(Fragment _item){
             page.add(_item);
         }
-
         @Override
         public Fragment getItem(int position) {
             return page.get(position);
         }
-
         @Override
         public int getCount() {
             return page.size();
         }
     }
+
 
 }
