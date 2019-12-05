ca2ac1ce5c96706d670dd22d8d36b68b3af5f549
alsrhkd77 <43804413+alsrhkd77@users.noreply.github.com>
2019-12-06 07:00:48 +0900

    html 파싱 async task 추가

--- app/src/main/java/com/project/daylight/LoadingActivity.java
+++ app/src/main/java/com/project/daylight/LoadingActivity.java
@@ -3,7 +3,11 @@ package com.project.daylight;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toolbar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 
